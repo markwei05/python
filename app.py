cb664caf2613e7334ff90dcb239025448cd58b6f
mark <markwei05@yahoo.ca>
2021-08-13 22:12:37 -0400

    test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return test homepage."""
-    json_data = {'Hello': 'World Christine!'}
+    json_data = {'Hello': 'World Christine wei!'}
     return jsonify(json_data)
 
 
